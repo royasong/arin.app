d74772e3d351edf694856a7c8e440a496e152184
roya song <roya.song@obigo.com>
2024-10-21 14:27:00 +0900

    open gallary

--- app/src/main/java/com/example/arin/MainActivity.kt
+++ app/src/main/java/com/example/arin/MainActivity.kt
@@ -19,78 +19,87 @@ import androidx.core.content.ContextCompat
 import android.content.pm.PackageManager
 import android.content.Intent
 import android.app.AlertDialog
+import android.util.Log
 
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResultLauncher
 
 //https://devgeek.tistory.com/12
+//출처: https://jwsoft91.tistory.com/278 [혀가 길지 않은 개발자:티스토리]
 class MainActivity : ComponentActivity() {
-    lateinit var ivProfile: ImageView
+    var TAG = "ARIN"
+
+    lateinit var ivProfile: Button
+    lateinit var bg_image: ImageView
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getActionBar()!!.setTitle("장아린 전용 앱")
         setContentView(R.layout.main_layout)
-        /*
-        enableEdgeToEdge()
-        setContent {
-            ArinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        bg_image = findViewById(R.id.ImageView_bg)
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intent = checkNotNull(result.data)
+                val imageUri = intent.data
+                Log.d(TAG, "user select " + imageUri)//
+                bg_image = findViewById(R.id.ImageView_bg)
+                bg_image.setImageURI(imageUri)
             }
         }
-        */
 
-        var change_background : Button = findViewById<Button>(R.id.btn_change_background)
-
-        change_background.setOnClickListener {
-            change_background.setText("Change")
-        }
+        initImageViewProfile()
     }
     private fun navigateGallery() {
+        //val intent = Intent(this, SubActivity::class.java)
         val intent = Intent(Intent.ACTION_PICK)
-        // 가져올 컨텐츠들 중에서 Image 만을 가져온다.
         intent.type = "image/*"
-        // 갤러리에서 이미지를 선택한 후, 프로필 이미지뷰를 수정하기 위해 갤러리에서 수행한 값을 받아오는 startActivityForeResult를 사용한다.
-        startActivityForResult(intent, 2000)
+        activityResultLauncher.launch(intent)
+
     }
     private fun showPermissionContextPopup() {
+        Log.d(TAG, "navigateGalleryshowPermissionContextPopup")
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다.")
             .setMessage("프로필 이미지를 바꾸기 위해서는 갤러리 접근 권한이 필요합니다.")
             .setPositiveButton("동의하기") { _, _ ->
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                requestPermissions(arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES), 1000)
             }
             .setNegativeButton("취소하기") { _, _ -> }
             .create()
             .show()
     }
     private fun initImageViewProfile() {
-        ivProfile = findViewById(R.id.ImageView_bg)
-
+        Log.d(TAG, "initImageViewProfile")
+        ivProfile = findViewById(R.id.ivProfile)
+        Log.d(TAG, "initImageViewProfile")
         ivProfile.setOnClickListener {
+            Log.d(TAG, "ivProfile.setOnClickListener execute")
             when {
                 // 갤러리 접근 권한이 있는 경우
                 ContextCompat.checkSelfPermission(
                     this,
-                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    android.Manifest.permission.READ_MEDIA_IMAGES
                 ) == PackageManager.PERMISSION_GRANTED
                 -> {
+                    Log.d(TAG, "ivProfile.setOnClickListener access gallary")
                     navigateGallery()
                 }
 
                 // 갤러리 접근 권한이 없는 경우 & 교육용 팝업을 보여줘야 하는 경우
-                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_MEDIA_IMAGES)
                 -> {
+                    Log.d(TAG, "ivProfile.setOnClickListener can't access gallary")
                     showPermissionContextPopup()
                 }
-
                 // 권한 요청 하기(requestPermissions) -> 갤러리 접근(onRequestPermissionResult)
-                else -> requestPermissions(
-                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                    1000
-                )
+                else -> {
+                    Log.d(TAG, "ivProfile.setOnClickListener requestPermissions")
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
+                        1000
+
+                    )
+                }
             }
 
         }
