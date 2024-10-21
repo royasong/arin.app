5e2879d033b544e2f1c41734e35be62f35c45bd1
roya song <roya.song@obigo.com>
2024-10-21 12:20:24 +0900

    backup

--- app/src/main/java/com/example/arin/MainActivity.kt
+++ app/src/main/java/com/example/arin/MainActivity.kt
@@ -10,15 +10,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import android.widget.Button
+import android.view.View
+import android.widget.ImageView
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arin.ui.theme.ArinTheme
+import androidx.core.content.ContextCompat
+import android.content.pm.PackageManager
+import android.content.Intent
+import android.app.AlertDialog
 
+
+//https://devgeek.tistory.com/12
 class MainActivity : ComponentActivity() {
+    lateinit var ivProfile: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getActionBar()!!.setTitle("장아린 전용 앱")
         setContentView(R.layout.main_layout)
-/*
+        /*
         enableEdgeToEdge()
         setContent {
             ArinTheme {
@@ -31,9 +41,66 @@ class MainActivity : ComponentActivity() {
             }
         }
         */
-    }
-}
 
+        var change_background : Button = findViewById<Button>(R.id.btn_change_background)
+
+        change_background.setOnClickListener {
+            change_background.setText("Change")
+        }
+    }
+    private fun navigateGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        // 가져올 컨텐츠들 중에서 Image 만을 가져온다.
+        intent.type = "image/*"
+        // 갤러리에서 이미지를 선택한 후, 프로필 이미지뷰를 수정하기 위해 갤러리에서 수행한 값을 받아오는 startActivityForeResult를 사용한다.
+        startActivityForResult(intent, 2000)
+    }
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("프로필 이미지를 바꾸기 위해서는 갤러리 접근 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+    private fun initImageViewProfile() {
+        ivProfile = findViewById(R.id.ImageView_bg)
+
+        ivProfile.setOnClickListener {
+            when {
+                // 갤러리 접근 권한이 있는 경우
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+                -> {
+                    navigateGallery()
+                }
+
+                // 갤러리 접근 권한이 없는 경우 & 교육용 팝업을 보여줘야 하는 경우
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                -> {
+                    showPermissionContextPopup()
+                }
+
+                // 권한 요청 하기(requestPermissions) -> 갤러리 접근(onRequestPermissionResult)
+                else -> requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000
+                )
+            }
+
+        }
+    }
+
+    public fun onClick1(v: View?) {
+        //setbackground
+    }
+
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
