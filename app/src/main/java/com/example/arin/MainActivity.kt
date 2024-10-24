4f9909a434b1f9c7ea14732fee73c02cf14a929a
roya song <roya.song@obigo.com>
2024-10-24 09:30:31 +0900

    save background file

--- app/src/main/java/com/example/arin/MainActivity.kt
+++ app/src/main/java/com/example/arin/MainActivity.kt
@@ -54,30 +54,22 @@ class MainActivity : ComponentActivity() {
             if (result.resultCode == RESULT_OK) {
                 val intent = checkNotNull(result.data)
                 var currentImageUri  = intent.data
-/*
-                val intent = checkNotNull(result.data)
-                val imageUri = intent.data
-                Log.d(TAG, "user select " + imageUri)//
-                Log.d(TAG, "path:" + imageUri!!.getPath().toString())
 
-                bg_image = findViewById(R.id.bg)
-                bg_image.setImageURI(imageUri)
-*/
-                try{
+                try {
                     currentImageUri?.let {
                         if(Build.VERSION.SDK_INT < 28) {
-                            Log.e(TAG, "MediaStore.Images.Media.getBitmap")//
                             val bitmap = MediaStore.Images.Media.getBitmap(
                                 this.contentResolver,
                                 currentImageUri
                             )
                             bg_image?.setImageBitmap(bitmap)
                         } else {
-
-                            Log.e(TAG, "ImageDecoder.createSource")//
                             val source = ImageDecoder.createSource(this.contentResolver, currentImageUri)
                             val bitmap = ImageDecoder.decodeBitmap(source)
                             bg_image?.setImageBitmap(bitmap)
+                            val back_dir_: String = getContext().getFilesDir().getPath()
+                            Log.e(TAG, "save dir : " + back_dir_)//
+                            saveBitmapAsFile(bitmap,back_dir_+"/arin_bg.png")
                         }
                     }
                 } catch(e : Exception) {
@@ -92,51 +84,7 @@ class MainActivity : ComponentActivity() {
     fun getContext(): Context {
         return context_
     }
-    /*
-    private fun saveBitmapImage(bitmap: Bitmap) {
-        val fileName =  "arin_background.png"
 
-        /*
-        * ContentValues() 객체 생성.
-        * ContentValues는 ContentResolver가 처리할 수 있는 값을 저장해둘 목적으로 사용된다.
-        * */
-        val contentValues = ContentValues()
-        contentValues.apply {
-            put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/ImageSave") // 경로 설정
-            put(MediaStore.Images.Media.DISPLAY_NAME, fileName) // 파일이름을 put해준다.
-            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
-            put(MediaStore.Images.Media.IS_PENDING, 1) // 현재 is_pending 상태임을 만들어준다.
-            // 다른 곳에서 이 데이터를 요구하면 무시하라는 의미로, 해당 저장소를 독점할 수 있다.
-        }
-
-        // 이미지를 저장할 uri를 미리 설정해놓는다.
-        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-        bg_string_ = uri.toString()
-        try {
-            if(uri != null) {
-                val image = contentResolver.openFileDescriptor(uri, "w", null)
-                // write 모드로 file을 open한다.
-
-                if(image != null) {
-                    val fos = FileOutputStream(image.fileDescriptor)
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
-                    //비트맵을 FileOutputStream를 통해 compress한다.
-                    fos.close()
-
-                    contentValues.clear()
-                    contentValues.put(MediaStore.Images.Media.IS_PENDING, 0) // 저장소 독점을 해제한다.
-                    contentResolver.update(uri, contentValues, null, null)
-                }
-            }
-        } catch(e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-     */
     private fun saveBitmapAsFile(bitmap: Bitmap, filepath: String) {
         val file = File(filepath)
         var os: OutputStream? = null
@@ -144,9 +92,7 @@ class MainActivity : ComponentActivity() {
         try {
             file.createNewFile()
             os = FileOutputStream(file)
-
             bitmap.compress(CompressFormat.PNG, 100, os)
-
             os.close()
         } catch (e: java.lang.Exception) {
             e.printStackTrace()
